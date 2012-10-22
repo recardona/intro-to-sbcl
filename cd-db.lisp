8c554d75eef2b065ae652c1e793f5756194ad49f
Rogelio E. Cardona-Rivera <me@rogel.io>
2012-10-22 17:19:59 -0400

    Added an update function and also included the toy db I'm using.

--- cd-db.lisp
+++ cd-db.lisp
@@ -111,3 +111,22 @@
       )
     )
 )
+
+;; Updates a set of records based on the selector-function
+;; that is passed in.
+(defun update (selector-fn
+               &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+        (mapcar
+         ;; Re-map the *db* variable
+         #'(lambda (row)
+             (when (funcall selector-fn row)
+               (if title    (setf (getf row :title) title))
+               (if artist   (setf (getf row :artist) artist))
+               (if rating   (setf (getf row :rating) rating))
+               (if ripped-p (setf (getf row :ripped) ripped))
+               )
+             row)
+         *db*)
+        )
+)
